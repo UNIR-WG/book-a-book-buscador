e3a0e73d9b050faa1379d24231f3c4487426c658
Mario Diaz <dev.mariodiaz@gmail.com>
2024-01-08 22:42:33 +0100

    Adding some important folders and functionalities

---
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/data/utils/SearchOperation.java
@@ -0,0 +1,12 @@
+package net.unir.missi.desarrollowebfullstack.bookabook.data.utils;
+
+public enum SearchOperation {
+    GREATER_THAN,
+    LESS_THAN,
+    GREATER_THAN_EQUAL,
+    LESS_THAN_EQUAL,
+    NOT_EQUAL,
+    EQUAL,
+    MATCH,
+    MATCH_END,
+}
---
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/data/utils/SearchStatement.java
@@ -0,0 +1,15 @@
+package net.unir.missi.desarrollowebfullstack.bookabook.data.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class SearchStatement {
+
+    private String key;
+    private Object value;
+    private SearchOperation operation;
+}
