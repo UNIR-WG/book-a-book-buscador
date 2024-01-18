b322b2131d6c9f3672c4e0d5b93f27622a291d3b
AleixMT <aleixaretra@gmail.com>
2024-01-18 18:56:47 +0100

    ADDED: feignclient

---
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/config/FeignClientConfig.java
@@ -0,0 +1,9 @@
+package net.unir.missi.desarrollowebfullstack.bookabook.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@EnableFeignClients
+@Configuration
+public class FeignClientConfig {
+}
--- src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/controller/HelloWorldController.java
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/controller/HelloWorldController.java
@@ -1,11 +1,13 @@
 package net.unir.missi.desarrollowebfullstack.bookabook.controller;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
 
+@FeignClient("spring-cloud-eureka-client")
 @RestController
 public class HelloWorldController {
 
@@ -13,10 +15,18 @@ public class HelloWorldController {
     public ResponseEntity<String> helloWorld()
     {
         try {
-            return ResponseEntity.ok("Hello World!");
+            return ResponseEntity.ok("Hello World from Operador!");
         } catch (Exception e) {
-            Logger.getGlobal().warning("Error saying hello" + e.getMessage());
+            Logger.getGlobal().warning("Error saying hello from operador" + e.getMessage());
             return ResponseEntity.internalServerError().build();
         }
     }
+
+    @GetMapping("/helloAnother")
+    public ResponseEntity<String> helloAnother()
+    {
+        // TODO: use spring application name of another service to call its /hello endpoint and return its value
+        return null;
+    }
+
 }
