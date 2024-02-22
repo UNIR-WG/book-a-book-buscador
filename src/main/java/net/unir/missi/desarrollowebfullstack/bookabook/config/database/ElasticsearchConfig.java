bd29ebddd9a07500a33700c4f9432998ded9c3a8
AleixMT <aleixaretra@gmail.com>
2024-02-22 12:45:04 +0100

    ADDED: elastic search configuration, changing JPA repositories to reactive elastic repositories

--- src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/config/database/ElasticsearchConfig.java
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/config/database/ElasticsearchConfig.java
@@ -1,26 +1,46 @@
 package net.unir.missi.desarrollowebfullstack.bookabook.config.database;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.erhlc.RestClients;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyStore;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "net.unir.missi.desarrollowebfullstack.bookabook.repository")
-@ComponentScan(basePackages = { "net.unir.missi.desarrollowebfullstack.bookabook.service" })
-public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
-    @Bean
+    /**
+     * Configures the ElasticSearch client
+     *
+     * @return Client instance to connect to our Elastic instance.
+     */
     @Override
-    public RestHighLevelClient elasticsearchClient() {
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedToLocalhost()
+                .usingSsl(ElasticsearchConfig.buildSSLContext())
+                .withBasicAuth("elatic", "chemistry")
                 .build();
+    }
 
-        return RestClients.create(clientConfiguration)
-                .rest();
+    /**
+     * Builds a dummy SSL context that trust all peers.
+     *
+     * @return Instance of SSLContext that trusts everyone.
+     */
+    private static SSLContext buildSSLContext()
+    {
+        try {
+            return new SSLContextBuilder().loadTrustMaterial((KeyStore) null, (TrustStrategy) TrustAllStrategy.INSTANCE).build();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/repository/ClientJpaRepository.java
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/repository/ClientJpaRepository.java
@@ -1,11 +1,13 @@
 package net.unir.missi.desarrollowebfullstack.bookabook.repository;
 
+import net.unir.missi.desarrollowebfullstack.bookabook.model.sql.Book;
 import net.unir.missi.desarrollowebfullstack.bookabook.model.sql.Client;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface ClientJpaRepository extends JpaRepository<Client, Long>, JpaSpecificationExecutor<Client> {
+interface ClientJpaRepository extends ElasticsearchRepository<Client, Long> {
 
 }
